b842604e0f21ee4df21ec5fb167caec7a1a698ce
ftnext <takuyafjp+develop@gmail.com>
2025-10-25 23:58:59 +0900

    [style] Fix F401 & TID252

--- src/emire/__init__.py
+++ src/emire/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: 2025-present ftnext <takuyafjp+develop@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from .extract import extract_json
-from .letter_cases import to_snake_case  # noqa: F401
-from .normalize import remove_spaces  # noqa: F401
+from emire.extract import extract_json
+from emire.letter_cases import to_snake_case
+from emire.normalize import remove_spaces
 
-__all__ = ["extract_json"]
+__all__ = ["extract_json", "to_snake_case", "remove_spaces"]
