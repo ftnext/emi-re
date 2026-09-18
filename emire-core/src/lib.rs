c79d5c6cd40bde1394baf667d0e195acac50ed74
ftnext <takuyafjp+develop@gmail.com>
2026-09-18 18:51:12 +0900

    refactor: Split rust core

--- emire-core/src/lib.rs
+++ emire-core/src/lib.rs
@@ -1,35 +1,3 @@
-use regex::Regex;
+mod extract;
 
-pub fn extract_json(markdown: &str) -> Option<&str> {
-    let re = Regex::new(r"(?s)```json\s*(.*?)\s*```").unwrap();
-    let captures = re.captures(markdown)?;
-    Some(captures.get(1)?.as_str())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_extract_json() {
-        let markdown = r#"
-Here is some text.
-```json
-{
-  "key": "value",
-  "number": 123
-}
-```
-More text here.
-"#;
-        assert_eq!(
-            extract_json(markdown),
-            Some(
-                r#"{
-  "key": "value",
-  "number": 123
-}"#
-            )
-        )
-    }
-}
+pub use extract::extract_json;
